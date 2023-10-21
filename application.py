3508c94a7cac25d53c51edc1d4735b05ef048ac3
rcf97 <rcf97@byu.edu>
2023-10-21 11:18:42 -0400

    websocket test

--- application.py
+++ application.py
@@ -1,17 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template
+
+from flask_socketio import SocketIO
 
 application = Flask(__name__)
 
+socketInstance = SocketIO(application)
 
 @application.route('/')
 def test():
-    return "<h>Clue-Less Test page.<br>Developed by Creative Engineers.</h>"
+    return render_template('index.html')
+    # return "<h>Clue-Less Test page.<br>Developed by Creative Engineers.</h>"
 
+@socketInstance.on('connect')
+def test_connect():
+    socketInstance.emit('after connect', {'data':'Connected to Flask Socket.'})
+
+@socketInstance.on('message')
+def handle_message(data):
+    print('received message: ' + data)
 
 
 #run the app
 if __name__ == "__main__":
     # Setting debug to True enables debug output. This line should be
     # removed before deploying a production app.
-    application.debug = True
-    application.run()
+    # application.debug = True
+    socketInstance.run(application, debug=True)
